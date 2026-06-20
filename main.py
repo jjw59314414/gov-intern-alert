98bdea1e28eccb6760a17ef60adec0f868952688
jjw59314414 <jjw59314414@gmail.com>
2026-06-21 01:35:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-raise Exception("HELLO TEST")
+import requests
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+r = requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    json={
+        "chat_id": CHAT_ID,
+        "text": "테스트"
+    }
+)
+
+print(r.status_code)
+print(r.text)
