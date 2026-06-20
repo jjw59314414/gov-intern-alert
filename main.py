063e91fbce3f5b36498d174eed96b3fe37c3d1c4
jjw59314414 <jjw59314414@gmail.com>
2026-06-21 01:38:39 +0900

    Replace requests with telegram bot for messaging

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import requests
+import telegram
 import os
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-r = requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    json={
-        "chat_id": CHAT_ID,
-        "text": "테스트"
-    }
-)
+bot = telegram.Bot(token=BOT_TOKEN)
 
-print(r.status_code)
-print(r.text)
+bot.send_message(
+    chat_id=CHAT_ID,
+    text="✅ GitHub Actions 테스트 성공"
+)
